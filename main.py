8343dcf57ab26c6e5b108ef13db54b2e225da073
VishwajitVBhat <vishwajitvenkatramanabhat@gmail.com>
2025-06-10 19:29:36 +0530

    09-06-2025

---
+++ main.py
@@ -0,0 +1,9 @@
+def is_in_set(my_set,item):
+    if len(my_set) < 10:    
+        for i in range(0,len(my_set)):
+            return my_set
+        
+my_set={1,2,3,4,5,6,7,8,9,}
+item=7
+
+print(is_in_set(my_set, item))
